d1a2b8c6d2bd6f42b9e343058d237ce5a2ad4702
Jean Viaene <jean.viaene@epfl.ch>
2022-06-10 15:01:11 +0200

    use a tabbed window app

--- android/app/src/main/java/com/epfl/dedis/hbt/MainActivity.kt
+++ android/app/src/main/java/com/epfl/dedis/hbt/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.epfl.dedis.hbt
 
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import com.epfl.dedis.hbt.ui.main.MainFragment
+import android.view.Menu
+import android.view.MenuItem
+import com.epfl.dedis.hbt.ui.main.SectionsPagerAdapter
+import com.epfl.dedis.hbt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 }
--- android/app/src/main/java/com/epfl/dedis/hbt/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.epfl.dedis.hbt.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
