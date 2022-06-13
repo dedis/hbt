681dbd8abe9329b169a2e0ee7e35cdfeb303f331
Jean Viaene <jean.viaene@epfl.ch>
2022-06-13 12:07:31 +0200

    Upgraded from Kotlin lang from 1.6.0 to 1.7.0 Add Login Fragment
    Signed-off-by: Jean Viaene <jean.viaene@epfl.ch>

--- android/app/src/main/java/com/epfl/dedis/hbt/ui/main/SectionsPagerAdapter.kt
+++ android/app/src/main/java/com/epfl/dedis/hbt/ui/main/SectionsPagerAdapter.kt
@@ -29,7 +29,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return TAB_TITLES.count()
     }
 }
