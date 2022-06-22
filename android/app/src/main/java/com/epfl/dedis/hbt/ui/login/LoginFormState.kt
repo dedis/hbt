877d1aa26a0e28d2a2f30a05d70d87a4a01fd55f
GabrielFleischer <gabriel.fleischer@epfl.ch>
2022-06-22 17:01:24 +0200

    Fix issue with registered user
    Signed-off-by: GabrielFleicher <gabriel.fleischer@epfl.ch>

--- android/app/src/main/java/com/epfl/dedis/hbt/ui/login/LoginFormState.kt
+++ android/app/src/main/java/com/epfl/dedis/hbt/ui/login/LoginFormState.kt
@@ -7,5 +7,5 @@ data class LoginFormState(
     val usernameError: Int? = null,
     val pincodeError: Int? = null,
     val isDataValid: Boolean = false,
-    val isUserRegistered: Boolean = false
+    val isUserRegistered: Boolean = true
 )
