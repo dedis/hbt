4bdd54485bce1a1d5ba2fda5b4cee3eb56d254a7
GabrielFleicher <gabriel.fleischer@epfl.ch>
2022-10-08 14:12:52 +0200

    Create test scenario for a register, logout and login back to the same user
    Signed-off-by: GabrielFleicher <gabriel.fleischer@epfl.ch>

---
+++ android/app/src/debug/java/com/epfl/dedis/hbt/test/ui/page/login/RegisterFragmentPage.kt
@@ -0,0 +1,21 @@
+package com.epfl.dedis.hbt.test.ui.page.login
+
+import androidx.annotation.IdRes
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.epfl.dedis.hbt.R
+
+object RegisterFragmentPage {
+
+    fun usernameInput(): ViewInteraction = onView(withId(R.id.registerUsername))
+
+    fun pincodeInput(): ViewInteraction = onView(withId(R.id.registerPincode))
+
+    fun passportInput(): ViewInteraction = onView(withId(R.id.registerPassport))
+
+    fun registerButton(): ViewInteraction = onView(withId(R.id.registerRegister))
+
+    @IdRes
+    fun registerFragmentId(): Int = R.id.registerFragment
+}
--- android/app/src/debug/java/com/epfl/dedis/hbt/test/ui/page/wallet/WalletFragmentPage.kt
+++ android/app/src/debug/java/com/epfl/dedis/hbt/test/ui/page/wallet/WalletFragmentPage.kt
@@ -1,5 +1,6 @@
 package com.epfl.dedis.hbt.test.ui.page.wallet
 
+import androidx.annotation.IdRes
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -8,4 +9,11 @@ import com.epfl.dedis.hbt.R
 object WalletFragmentPage {
 
     fun logout(): ViewInteraction = onView(withId(R.id.walletButtonLogout))
+
+    fun username(): ViewInteraction = onView(withId(R.id.walletName))
+
+    fun role(): ViewInteraction = onView(withId(R.id.walletRole))
+
+    @IdRes
+    fun walletFragmentId() = R.id.walletFragment
 }
