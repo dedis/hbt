52cc31b8bd1550e6abb11cccda4572811e89b55c
GabrielFleicher <gabriel.fleischer@epfl.ch>
2022-11-12 12:56:51 +0100

    Add scanner and preview in ScanFragment

--- android/app/src/main/java/com/epfl/dedis/hbt/ui/wallet/ScanFragment.kt
+++ android/app/src/main/java/com/epfl/dedis/hbt/ui/wallet/ScanFragment.kt
@@ -1,17 +1,32 @@
 package com.epfl.dedis.hbt.ui.wallet
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.epfl.dedis.hbt.R
 import com.epfl.dedis.hbt.data.model.Role
 import com.epfl.dedis.hbt.databinding.FragmentWalletScanBinding
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.ExecutorService
+
 
 @AndroidEntryPoint
 class ScanFragment : Fragment() {
@@ -23,17 +38,36 @@ class ScanFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var barcodeScanner: BarcodeScanner
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Create a request permission launcher which will ask for permission when launched
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+            requireActivity().activityResultRegistry
+        ) {
+            // This is the callback of the permission granter
+            applyPermissionToView()
+        }
+
         _binding = FragmentWalletScanBinding.inflate(inflater, container, false).apply {
             walletName.text = walletViewModel.user?.name.toString()
             val role = walletViewModel.user?.role ?: Role.BENEFICIARY
             walletRole.text = getString(role.roleName)
             walletBalance.text =
                 getString(R.string.hbt_currency, walletViewModel.wallet?.balance ?: 0.0f)
+
+            requestPermissionButton.setOnClickListener {
+                // Launch the permission request on click
+                requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA
+                )
+            }
         }
 
         // Override back button such that it cancels current transaction
@@ -70,4 +104,68 @@ class ScanFragment : Fragment() {
                 }
             })
     }
+
+    override fun onResume() {
+        super.onResume()
+        applyPermissionToView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+        barcodeScanner.close()
+    }
+
+    private fun startCamera() {
+        val cameraController = LifecycleCameraController(requireContext())
+        val previewView: PreviewView = binding.previewView
+
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        barcodeScanner = BarcodeScanning.getClient(options)
+
+        cameraController.setImageAnalysisAnalyzer(
+            ContextCompat.getMainExecutor(requireActivity()),
+            MlKitAnalyzer(
+                listOf(barcodeScanner),
+                COORDINATE_SYSTEM_VIEW_REFERENCED,
+                ContextCompat.getMainExecutor(requireActivity())
+            ) { result: MlKitAnalyzer.Result? ->
+                val barcodeResults = result?.getValue(barcodeScanner)
+                // Test result value
+                if ((barcodeResults == null) ||
+                    (barcodeResults.size == 0) ||
+                    (barcodeResults.first() == null)
+                ) {
+                    return@MlKitAnalyzer
+                }
+
+                onResult(barcodeResults[0])
+            }
+        )
+
+        cameraController.bindToLifecycle(this)
+        previewView.controller = cameraController
+    }
+
+    private fun onResult(barcode: Barcode) {
+        TODO("Not yet implemented")
+    }
+
+    private fun applyPermissionToView() {
+        // Depending on the current permission state,
+        // show the preview or the request permission views
+        if (checkPermission()) {
+            binding.requestPermission.visibility = View.GONE
+            binding.previewView.visibility = View.VISIBLE
+            startCamera()
+        } else {
+            binding.requestPermission.visibility = View.VISIBLE
+            binding.previewView.visibility = View.GONE
+        }
+    }
+
+    private fun checkPermission() =
+        checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PERMISSION_GRANTED
 }
