2bce92249f00f2834ffba51bc9b9844cb299350b
GabrielFleicher <gabriel.fleischer@epfl.ch>
2022-12-01 19:16:56 +0100

    Implement basic feature of Data Fragment
    Signed-off-by: GabrielFleicher <gabriel.fleischer@epfl.ch>

--- android/app/src/main/java/com/epfl/dedis/hbt/ui/register/PassportDataFragment.kt
+++ android/app/src/main/java/com/epfl/dedis/hbt/ui/register/PassportDataFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.epfl.dedis.hbt.databinding.FragmentPassportDataBinding
+import com.epfl.dedis.hbt.service.passport.mrz.MRZInfo
+import com.epfl.dedis.hbt.ui.MainActivity
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +20,25 @@ class PassportDataFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         return FragmentPassportDataBinding.inflate(inflater, container, false).apply {
+            automaticScan.setOnClickListener {
+                MainActivity.setCurrentFragment(
+                    parentFragmentManager,
+                    ScanPassportFragment()
+                )
+            }
 
+            validate.setOnClickListener {
+                MainActivity.setCurrentFragment(
+                    parentFragmentManager,
+                    NFCPassportFragment.newInstance(
+                        MRZInfo(
+                            passportNumber.text.toString(),
+                            editBirthDate.text.toString(),
+                            editExpirationDate.text.toString()
+                        )
+                    )
+                )
+            }
         }.root
     }
 }
