e5c33e87ba35bd5c6b681b5964e4a4d967e4a03b
GabrielFleicher <gabriel.fleischer@epfl.ch>
2022-12-05 15:35:32 +0100

    Fix tests
    Signed-off-by: GabrielFleicher <gabriel.fleischer@epfl.ch>

---
+++ android/app/src/debug/java/com/epfl/dedis/hbt/test/ui/page/register/ScanPassportFragmentPage.kt
@@ -0,0 +1,10 @@
+package com.epfl.dedis.hbt.test.ui.page.register
+
+import androidx.annotation.IdRes
+import com.epfl.dedis.hbt.R
+
+object ScanPassportFragmentPage {
+
+    @IdRes
+    fun scanPassportFragmentId() = R.id.scanPassportFragment
+}
