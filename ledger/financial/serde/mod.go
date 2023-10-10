b8bdc6b8995dd9e6d61d6ba5616286fc388baaea
Jean <jean.viaene@epfl.ch>
2023-10-10 21:04:38 +0200

    remove ledger

--- ledger/financial/serde/mod.go
+++
@@ -1,7 +0,0 @@
-package serde
-
-// Serde abstracts away the serialization/deserialization mechanism
-type Serde interface {
-	Marshal(v any) ([]byte, error)
-	Unmarshal(buff []byte, v any) error
-}
